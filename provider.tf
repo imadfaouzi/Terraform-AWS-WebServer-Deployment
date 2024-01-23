7dc9d7599bc5b733b5725b3756eb98055658d336
imadfaouzi <faouziimad2001@gmail.com>
2024-01-23 13:32:31 +0100

    first commit by imad

---
+++ provider.tf
@@ -0,0 +1,14 @@
+# this  is a provider file which is used to configure the provider
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "4.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  profile = "default"
+}
